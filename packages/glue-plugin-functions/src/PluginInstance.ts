d4ba53d57436f1978a20c561fe3a4b6657da2ea9
vidhi499 <katariavidhi99@gmail.com>
2023-04-03 17:01:10 +0530

    feat: functions plugin added

---
+++ packages/glue-plugin-functions/src/PluginInstance.ts
@@ -0,0 +1,54 @@
+import AppCLI from "@gluestack-v2/framework-cli/build/helpers/lib/app";
+
+import IPlugin from "@gluestack-v2/framework-cli/build/types/plugin/interface/IPlugin";
+import IGlueStorePlugin from "@gluestack-v2/framework-cli/build/types/store/interface/IGluePluginStore";
+import BaseGluestackPluginInstance from "@gluestack-v2/framework-cli/build/types/gluestack-plugin-instance";
+
+export class PluginInstance extends BaseGluestackPluginInstance {
+  app: AppCLI;
+  name: string;
+  callerPlugin: IPlugin;
+  isOfTypeInstance: boolean = false;
+  gluePluginStore: IGlueStorePlugin;
+  installationPath: string;
+
+  constructor(
+    app: AppCLI,
+    callerPlugin: IPlugin,
+    name: string,
+    gluePluginStore: IGlueStorePlugin,
+    installationPath: string
+  ) {
+    super(app, callerPlugin, name, gluePluginStore, installationPath);
+
+    this.app = app;
+    this.name = name;
+    this.callerPlugin = callerPlugin;
+    this.gluePluginStore = gluePluginStore;
+    this.installationPath = installationPath;
+  }
+
+  init() {
+    //
+  }
+
+  destroy() {
+    //
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getCallerPlugin(): IPlugin {
+    return this.callerPlugin;
+  }
+
+  getInstallationPath(): string {
+    return this.installationPath;
+  }
+
+  watch(): string[] {
+    return ["services", "public", "test"];
+  }
+}
---
+++ packages/glue-plugin-functions/src/index.ts
@@ -0,0 +1,107 @@
+// @ts-ignore
+import packageJSON from "../package.json";
+import { PluginInstance } from "./PluginInstance";
+
+import AppCLI from "@gluestack-v2/framework-cli/build/helpers/lib/app";
+import BaseGluestackPlugin from "@gluestack-v2/framework-cli/build/types/gluestack-plugin";
+
+import IPlugin from "@gluestack-v2/framework-cli/build/types/plugin/interface/IPlugin";
+import IInstance from "@gluestack-v2/framework-cli/build/types/plugin/interface/IInstance";
+import IGlueStorePlugin from "@gluestack-v2/framework-cli/build/types/store/interface/IGluePluginStore";
+import { reWriteFile } from "./helpers/rewrite-file";
+import { Workspaces } from "@gluestack/helpers";
+
+// Do not edit the name of this class
+export class GlueStackPlugin extends BaseGluestackPlugin {
+  app: AppCLI;
+  instances: IInstance[];
+  type: "stateless" | "stateful" | "devonly" = "devonly";
+  gluePluginStore: IGlueStorePlugin;
+
+  constructor(app: AppCLI, gluePluginStore: IGlueStorePlugin) {
+    super(app, gluePluginStore);
+
+    this.app = app;
+    this.instances = [];
+    this.gluePluginStore = gluePluginStore;
+  }
+
+  init() {
+    // this.app.addEventListener("booting.web", (...args: any[]): void => {
+    //   console.log({ message: "booting web event listener", args });
+    //   console.log(this.gluePluginStore.get("message"));
+    //   this.gluePluginStore.set("message", "Hello from function plugin");
+    //   console.log(this.gluePluginStore.get("message"));
+    // });
+  }
+
+  destroy() {
+    //
+  }
+
+  getName(): string {
+    return packageJSON.name;
+  }
+
+  getVersion(): string {
+    return packageJSON.version;
+  }
+
+  getType(): "stateless" | "stateful" | "devonly" {
+    return this.type;
+  }
+
+  // @ts-ignore
+  getTemplateFolderPath(): string {
+    return `${process.cwd()}/node_modules/${this.getName()}/template`;
+  }
+
+  getInstallationPath(target: string): string {
+    return `./${target}`;
+  }
+
+  async runPostInstall(instanceName: string, target: string) {
+    const instance: IInstance = await this.app.createPluginInstance(
+      this,
+      instanceName,
+      this.getTemplateFolderPath(),
+      target
+    );
+
+    if (!instance) {
+      return;
+    }
+
+    // // rewrite router.js with the installed instance name
+    // const routerFile = `${instance.getInstallationPath()}/router.js`;
+    // await reWriteFile(routerFile, removeSpecialChars(instanceName), 'INSTANCENAME');
+
+    // update package.json'S name index with the new instance name
+    const pluginPackage = `${instance.getInstallationPath()}/package.json`;
+    await reWriteFile(pluginPackage, instanceName, "INSTANCENAME");
+
+    // update root package.json's workspaces with the new instance name
+    const rootPackage = `${process.cwd()}/package.json`;
+    await Workspaces.append(rootPackage, instance.getInstallationPath());
+  }
+
+  createInstance(
+    key: string,
+    gluePluginStore: IGlueStorePlugin,
+    installationPath: string
+  ): IInstance {
+    const instance = new PluginInstance(
+      this.app,
+      this,
+      key,
+      gluePluginStore,
+      installationPath
+    );
+    this.instances.push(instance);
+    return instance;
+  }
+
+  getInstances(): IInstance[] {
+    return this.instances;
+  }
+}
