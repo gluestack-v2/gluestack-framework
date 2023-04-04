b56fa5bac4c323c0eee18d445764f017be0617f9
Rohit Singh <rohit99.ind@gmail.com>
2023-04-04 13:11:37 +0530

    feat: functions plugin

--- packages/glue-plugin-functions/src/PluginInstance.ts
+++ packages/glue-plugin-functions/src/PluginInstance.ts
@@ -49,6 +49,6 @@ export class PluginInstance extends BaseGluestackPluginInstance {
   }
 
   watch(): string[] {
-    return ["services", "public", "test"];
+    return ["funcs"];
   }
 }
---
+++ packages/glue-plugin-functions/template/funcs/add.js
@@ -0,0 +1,4 @@
+export function add(a, b) {
+  console.log(a + b);
+  return a + b;
+}
