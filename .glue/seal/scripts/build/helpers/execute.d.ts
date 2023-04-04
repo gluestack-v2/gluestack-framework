6add8bfbeee0364f5200b3f66be0a74137d44aeb
Faiz A. Farooqui <faizz.af@gmail.com>
2023-04-04 18:16:46 +0530

    chore: removed seal scripts build

--- .glue/seal/scripts/build/helpers/execute.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const execute: (command: string, args: string[], options: any) => Promise<unknown>;
--- .glue/seal/scripts/build/helpers/typeof.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * typeOf returns the type of the value passed in.
- *
- * @param value any
- * @returns string
- */
-export declare const typeOf: (value: any) => string;
