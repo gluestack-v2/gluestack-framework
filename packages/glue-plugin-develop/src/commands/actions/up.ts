769789402ab0420b4e526efed6b355c3a5c83948
Rohit Singh <rohit99.ind@gmail.com>
2023-04-14 15:45:50 +0530

    feat: platform support

--- packages/glue-plugin-develop/src/commands/actions/up.ts
+++ packages/glue-plugin-develop/src/commands/actions/up.ts
@@ -1,17 +1,21 @@
-import { Workspaces } from "@gluestack/helpers";
 import AppCLI from "@gluestack-v2/framework-cli/build/helpers/lib/app";
 import {
-  error,
   success,
   warning,
+  error,
 } from "@gluestack-v2/framework-cli/build/helpers/print";
-
-import { FOLDER_STRUCTURE } from "../../constants/folder-structure";
-import createFoldersFromJson from "../../helpers/create-folders-from-json";
-import { GLUE_GENERATED_PACKAGES_PATH } from "../../constants/glue-generated-packages";
 import { spawn } from "child_process";
 
-async function upSealService(serviceName: string, servicePlatform: string) {
+function upSealService(serviceName: string, opts: any) {
+  let servicePlatform = "local";
+
+  if (Object.keys(opts).length > 0) {
+    if (opts.platform) {
+      servicePlatform = opts.platform;
+    } else {
+      servicePlatform = opts.p;
+    }
+  }
   const SEAL_SERVICES_PATH: string = ".glue/__generated__/seal/services/";
   const sealUp = spawn("sh", [
     "-c",
@@ -21,48 +25,22 @@ async function upSealService(serviceName: string, servicePlatform: string) {
   sealUp.stdout.on("data", (data) => {
     success(`${data}`);
   });
+
   sealUp.stderr.on("data", (data) => {
     error(`${data}`);
   });
-
-  // console.log(sealUp.status);
-
-  // success("asf", `kasbkja${sealUp.status}`);
-
-  // error(sealUp.stderr.toString());
 }
 
-export default async (app: AppCLI): Promise<void> => {
-  // creates folders from FOLDER_STRUCTURE constant
-  // await createFoldersFromJson(FOLDER_STRUCTURE, process.cwd());
-
-  // add __generated__/packages into workspaces
-  // await Workspaces.append(
-  //   `${process.cwd()}/package.json`,
-  //   GLUE_GENERATED_PACKAGES_PATH
-  // );
-
-  // builds plugins
+export default async (app: AppCLI, opts: any): Promise<void> => {
   for await (const plugin of app.plugins) {
-    // if (!plugin.build) {
-    //   warning(`${plugin.getName()}`, "contains no build method, skipping...");
-    //   continue;
-    // }
     let instances = plugin.getInstances();
     for (let instance of instances) {
       // @ts-ignore
       if (plugin.sealInit) {
         success("Seal service plugin found!");
-        await upSealService(instance.getName(), "local");
-        warning(plugin.getName());
+        upSealService(instance.getName(), opts);
+        warning(plugin.getName(), "NAME");
       }
     }
-
-    // try {
-    //   await plugin.build();
-    // } catch (e) {
-    //   console.log('>>>>', e);
-    //   error(plugin.getName(), "build failed");
-    // }
   }
 };
--- packages/glue-plugin-develop/src/commands/up.ts
+++ packages/glue-plugin-develop/src/commands/up.ts
@@ -6,6 +6,7 @@ import action from "./actions/up";
 export default async (program: any, app: AppCLI): Promise<void> => {
   const command: ICommand = program
     .command("up")
+    .option("-p <platform>", "Choose Platform")
     .description("Deploys all the plugins to the docker")
-    .action(() => action(app));
+    .action((opts: any) => action(app, opts));
 };
