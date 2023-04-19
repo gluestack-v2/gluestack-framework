08c822d1b377e4880b9c78328391a60c0e74cfae
Madhav Bansal <madhavbansal@Madhavs-MacBook-Air.local>
2023-04-19 18:14:59 +0530

    added database plugin

---
+++ packages/glue-plugin-database/src/PluginInstance.ts
@@ -0,0 +1,51 @@
+
+import AppCLI from '@gluestack-v2/framework-cli/build/helpers/lib/app';
+
+import IPlugin from '@gluestack-v2/framework-cli/build/types/plugin/interface/IPlugin';
+import IGlueStorePlugin from '@gluestack-v2/framework-cli/build/types/store/interface/IGluePluginStore';
+import BaseGluestackPluginInstance from '@gluestack-v2/framework-cli/build/types/gluestack-plugin-instance';
+
+export class PluginInstance extends BaseGluestackPluginInstance {
+  app: AppCLI;
+  name: string;
+  callerPlugin: IPlugin;
+  isOfTypeInstance: boolean = false;
+  gluePluginStore: IGlueStorePlugin;
+  installationPath: string;
+
+  constructor(
+    app: AppCLI,
+    callerPlugin: IPlugin,
+    name: string,
+    gluePluginStore: IGlueStorePlugin,
+    installationPath: string
+  ) {
+    super(app, callerPlugin, name, gluePluginStore, installationPath);
+
+    this.app = app;
+    this.name = name;
+    this.callerPlugin = callerPlugin;
+    this.gluePluginStore = gluePluginStore;
+    this.installationPath = installationPath;
+  }
+
+  init() {
+    //
+  }
+
+  destroy() {
+    //
+  }
+
+  watch(): string[] {
+    return [];
+  }
+
+  getDockerfile(): string {
+		return `${this.getInstallationPath()}/Dockerfile`;
+	}
+
+	getSealServicefile(): string {
+		return `${this.getInstallationPath()}/seal.service.yaml`;
+	}
+}
