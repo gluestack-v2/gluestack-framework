ce3201c803ef8042da767bc99d78022738403970
Suraj <surajahmedchoudhury@Surajs-MacBook-Air.local>
2023-05-02 17:53:49 +0530

    fix: updated server sdk

--- packages/glue-plugin-service-gateway/template/serverSdk.js
+++ packages/glue-plugin-service-gateway/template/serverSdk.js
@@ -12,19 +12,26 @@ class Ctx {
 	}
 
 	events = {
-		emit: (eventName, params) => {
+		emit: (eventName, params, data) => {
+			console.log(eventName, params, data);
 			return this._ctx.call("events.emit", {
-				eventName,
-				params,
+				data: { eventName: eventName, params: params, data: data },
 			});
-			// return this._ctx.call("events.emit", {
-			// 	data: { eventName: eventName, params: params, data: data },
-			// });
 		},
 	};
 
-	queue = {
-		push: () => {},
+	queues = {
+		push: (functionName, params) => {
+			// Uncomment to debug
+			// console.log("Queues Data", {
+			// 	functionName: functionName,
+			// 	params: params,
+			// });
+
+			return this._ctx.call("queues.push", {
+				data: { functionName: functionName, params: params },
+			});
+		},
 	};
 }
 
