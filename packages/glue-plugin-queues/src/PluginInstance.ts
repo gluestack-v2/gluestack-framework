a816a669a671b6aba98b5d99cb579bc813dca25a
Madhav Bansal <madhavbansal@Madhavs-MacBook-Air.local>
2023-05-02 19:40:44 +0530

    fix: build

--- packages/glue-plugin-queues/src/PluginInstance.ts
+++ packages/glue-plugin-queues/src/PluginInstance.ts
@@ -1,13 +1,9 @@
-import fs, { unlinkSync } from "fs";
 import AppCLI from "@gluestack-v2/framework-cli/build/helpers/lib/app";
 
 import IPlugin from "@gluestack-v2/framework-cli/build/types/plugin/interface/IPlugin";
 import IGlueStorePlugin from "@gluestack-v2/framework-cli/build/types/store/interface/IGluePluginStore";
 import BaseGluestackPluginInstance from "@gluestack-v2/framework-cli/build/types/BaseGluestackPluginInstance";
 import IInstance from "@gluestack-v2/framework-cli/build/types/plugin/interface/IInstance";
-import { join } from "path";
-import fileExists from "./helpers/file-exists";
-import writeFile from "./helpers/write-file";
 
 export class PluginInstance extends BaseGluestackPluginInstance {
   app: AppCLI;
@@ -49,21 +45,23 @@ export class PluginInstance extends BaseGluestackPluginInstance {
     return `${this._destinationPath}/seal.service.yaml`;
   }
 
-
-
   getGatewayInstanceInfo() {
     const plugin: IPlugin | null = this.app.getPluginByName(
       "@gluestack-v2/glue-plugin-service-gateway"
     );
 
     if (!plugin) {
-      console.error(`Plugin "@gluestack-v2/glue-plugin-service-gateway" not found.`);
+      console.error(
+        `Plugin "@gluestack-v2/glue-plugin-service-gateway" not found.`
+      );
       return "";
     }
 
     const instances: Array<IInstance> | undefined = plugin.instances;
     if (!instances || instances.length <= 0) {
-      console.error(`No instance with "@gluestack-v2/glue-plugin-service-gateway" found.`);
+      console.error(
+        `No instance with "@gluestack-v2/glue-plugin-service-gateway" found.`
+      );
       return "";
     }
 
@@ -75,20 +73,15 @@ export class PluginInstance extends BaseGluestackPluginInstance {
       "@gluestack-v2/glue-plugin-service-gateway"
     ) as IPlugin;
 
-
     if (!plugin) {
       return;
     }
 
     // @ts-ignore
     plugin.generateQueuesService(this.getName());
-
   }
 
   async build(): Promise<void> {
-
     this.generateQueuesInServiceGateway();
-
   }
-
 }
