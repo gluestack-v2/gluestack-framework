9f50f61b665c54f46de955d5f5c0ccef00297851
Suraj <surajahmedchoudhury@Surajs-MacBook-Air.local>
2023-06-06 17:59:55 +0530

    fix: config updated

--- config/index.ts
+++ config/index.ts
@@ -1,16 +1,3 @@
-class Auth {
-  constructor() {
-    this.login();
-  }
-  init() {
-    console.log('Auth init');
-  }
-  login() {
-    console.log('login');
-  }
-}
 export const config = {
-  providers: {
-    auth: Auth,
-  },
+  providers: {},
 };
