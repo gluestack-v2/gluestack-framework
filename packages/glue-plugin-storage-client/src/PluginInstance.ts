9dfa8a0dad164c5406941d7c054ee3377efd57f1
Viraj-10 <virajjoshi132@gmail.com>
2023-07-18 18:21:06 +0530

    fix: refactor storage client

--- packages/glue-plugin-storage-client/src/PluginInstance.ts
+++ packages/glue-plugin-storage-client/src/PluginInstance.ts
@@ -2,20 +2,12 @@ import AppCLI from '@gluestack-v2/framework-cli/build/helpers/lib/app';
 
 import IPlugin from '@gluestack-v2/framework-cli/build/types/plugin/interface/IPlugin';
 import IGlueStorePlugin from '@gluestack-v2/framework-cli/build/types/store/interface/IGluePluginStore';
-import chokidar from 'chokidar';
 import IInstance from '@gluestack-v2/framework-cli/build/types/plugin/interface/IInstance';
-import path1, { join } from 'path';
-import fs, { unlinkSync } from 'fs';
-import writeFile from './helpers/write-file';
-import fileExists from './helpers/file-exists';
 import BaseGluestackPluginInstance from '@gluestack-v2/framework-cli/build/types/BaseGluestackPluginInstance';
-import { GLUE_GENERATED_SEAL_SERVICES_PATH } from '@gluestack-v2/framework-cli/build/constants/gluestack.v2';
 import { writeStorageClient } from './helpers/write-storage-client';
-import {
-  GLUE_GENERATED_PACKAGES_PATH,
-  GLUE_GENERATED_SEAL_SERVICES_PATH,
-} from '@gluestack-v2/framework-cli/build/constants/gluestack.v2';
+import { GLUE_GENERATED_PACKAGES_PATH } from '@gluestack-v2/framework-cli/build/constants/gluestack.v2';
 import { writeStorageServerSdk } from './helpers/write-storage-server-sdk';
+import { join } from 'path';
 
 export class PluginInstance extends BaseGluestackPluginInstance {
   app: AppCLI;
@@ -54,15 +46,14 @@ export class PluginInstance extends BaseGluestackPluginInstance {
   }
 
   getSealServicefile(): string {
-    return `${this._destinationPath}/seal.service.yaml`;
+    return join(this._destinationPath, 'bolt.service.yaml');
   }
 
   getSourcePath(): string {
-    return `${process.cwd()}/server/${this.getName()}`;
+    return join(process.cwd(), 'server', this.getName());
   }
 
   async build() {
-    // await this.app.write(this._sourcePath, this._destinationPath);
     await this.writeStorageService();
     const clientPackagePath = join(
       GLUE_GENERATED_PACKAGES_PATH,
@@ -79,8 +70,6 @@ export class PluginInstance extends BaseGluestackPluginInstance {
     await this.app.createPackage(`${this.getName()}-server-sdk`, sdkPath);
 
     await writeStorageServerSdk(this.getName(), serverPackagePath);
-
-    // await this.writeSdkForStorageClient();
   }
 
   async watch(callback?: any) {
@@ -118,16 +107,6 @@ export class PluginInstance extends BaseGluestackPluginInstance {
     }
     // @ts-ignore
     plugin.generateStorageClientService(this.getName());
-    // const envData = {
-    //   endPoint: this.settings.endPoint,
-    //   port: this.settings.port,
-    //   useSSL: this.settings.useSSL,
-    //   accessKey: this.settings.accessKey,
-    //   secretKey: this.settings.secretKey,
-    //   // region: this.settings.region,
-    //   // transport: this.settings.transport,
-    //   // sessionToken: this.settings.sessionToken,
-    // };
   }
 
   getGatewayInstanceInfo() {
@@ -152,17 +131,4 @@ export class PluginInstance extends BaseGluestackPluginInstance {
 
     return instances[0].getName();
   }
-  // // Override getDestinationPath
-  // getDestinationPath() {
-  //   const gatewayInstanceName: string = this.getGatewayInstanceInfo();
-
-  //   return join(
-  //     process.cwd(),
-  //     GLUE_GENERATED_SEAL_SERVICES_PATH,
-  //     gatewayInstanceName,
-  //     "src",
-  //     gatewayInstanceName,
-  //     this.getName()
-  //   );
-  // }
 }
