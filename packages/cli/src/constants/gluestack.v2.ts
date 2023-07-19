fca9eaad70a61bac22fb35b6664f8113ab579100
vidhi499 <katariavidhi99@gmail.com>
2023-07-19 12:39:15 +0530

    fix: constants

--- packages/cli/src/constants/gluestack.v2.ts
+++ packages/cli/src/constants/gluestack.v2.ts
@@ -4,6 +4,11 @@ export const GLUE_GENERATED_PATH = '.glue/__generated__';
 
 export const GLUE_GENERATED_PACKAGES_PATH = `${GLUE_GENERATED_PATH}/packages`;
 
-export const GLUE_GENERATED_SEAL_PATH = `${GLUE_GENERATED_PATH}/seal`;
+export const GLUE_GENERATED_SEAL_PATH = `${GLUE_GENERATED_PATH}`;
 
 export const GLUE_GENERATED_SEAL_SERVICES_PATH = `${GLUE_GENERATED_SEAL_PATH}/services`;
+
+export const SEAL_SERVICES_PATH: string =
+	'.glue/__generated__/services' as const;
+
+export const GLUE_CONFIG_PATH = './config';
