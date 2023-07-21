d502d8cb136985e2adefaa682cb241e1855ec924
Viraj-10 <virajjoshi132@gmail.com>
2023-07-21 18:34:34 +0530

    fix: type issues for database-client

--- packages/glue-plugin-database-client/src/commands/actions/db.ts
+++ packages/glue-plugin-database-client/src/commands/actions/db.ts
@@ -1,16 +1,12 @@
-import { readFileSync, writeFileSync, existsSync, rmSync } from "fs";
-import AppCLI from "@gluestack-v2/framework-cli/build/helpers/lib/app";
-import { error } from "@gluestack-v2/framework-cli/build/helpers/print";
-import { fileExists } from "@gluestack/helpers";
-import { log } from "console";
+import { AppCLI, error } from '@gluestack-v2/framework-cli';
 
 export default async (app: AppCLI, argument: any): Promise<void> => {
   let dbclientPlugin = app.getPluginByName(
-    "@gluestack-v2/glue-plugin-database-client"
+    '@gluestack-v2/glue-plugin-database-client'
   );
   let dbclientInstance = dbclientPlugin?.getInstances()[0];
   if (!dbclientInstance) {
-    error("No database client instance found");
+    error('No database client instance found');
     return;
   }
   let generatePath = `${dbclientInstance._destinationPath}`;
--- packages/glue-plugin-database-client/src/commands/db.ts
+++ packages/glue-plugin-database-client/src/commands/db.ts
@@ -1,15 +1,16 @@
-import AppCLI from "@gluestack-v2/framework-cli/build/helpers/lib/app";
-import { ICommand } from "@gluestack-v2/framework-cli/build/types/helpers/interface/ICommandCallback";
+import { AppCLI } from '@gluestack-v2/framework-cli';
+import type { ICommand } from '@gluestack-v2/framework-cli';
 
-import action from "./actions/db";
+import action from './actions/db';
 
 export default async (program: any, app: AppCLI): Promise<void> => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const command: ICommand = program
-    .command("dbclient <type>")
+    .command('dbclient <type>')
     .allowUnknownOption()
-    .option("type --options <opts>")
+    .option('type --options <opts>')
     .description(
-      "Prisma options. Please refer => https://www.prisma.io/docs/reference/api-reference/command-reference"
+      'Prisma options. Please refer => https://www.prisma.io/docs/reference/api-reference/command-reference'
     )
     .action((type: string) => {
       return action(app, { type, opts: program.args });
